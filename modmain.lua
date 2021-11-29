d6a3b8d4071a3406835528b87b4e7967951f1933
Cloudy Young <cloudy.young@outlook.com>
2021-11-29 09:07:19 -0700

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -28,110 +28,36 @@ AddRecipe(
     "images/city_lamp.xml"
 )
 
-local STRINGS = GLOBAL.STRINGS
+local LANGUAGE = _G.LANGUAGE
 
-if STRINGS.CHARACTERS.WALANI == nil then
-    STRINGS.CHARACTERS.WALANI = {
-        DESCRIBE = {}
-    }
-end -- DLC002
-if STRINGS.CHARACTERS.WARLY == nil then
-    STRINGS.CHARACTERS.WARLY = {
-        DESCRIBE = {}
-    }
-end -- DLC002
-if STRINGS.CHARACTERS.WOODLEGS == nil then
-    STRINGS.CHARACTERS.WOODLEGS = {
-        DESCRIBE = {}
-    }
-end -- DLC002
-if STRINGS.CHARACTERS.WILBA == nil then
-    STRINGS.CHARACTERS.WILBA = {
-        DESCRIBE = {}
-    }
-end -- DLC003
-if STRINGS.CHARACTERS.WARBUCKS == nil then
-    STRINGS.CHARACTERS.WARBUCKS = {
-        DESCRIBE = {}
-    }
-end -- DLC003
 
--- Prefab
-STRINGS.NAMES.CITY_LAMP = "Lamp Post"
-STRINGS.RECIPE_DESC.CITY_LAMP = "I can't believe I can make this."
 
--- Characters
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Plain fire. No science involved.",
-    ON = "Lusterless."
+local localizations = 
+{
+    {id = LANGUAGE.FRENCH,          alt_id = nil,                   strings = "french" },
+    {id = LANGUAGE.SPANISH,         alt_id = LANGUAGE.SPANISH_LA,   strings = "spanish" },
+    -- {id = LANGUAGE.SPANISH_LA,      alt_id = nil,                   strings = "spanish_mex" },
+    {id = LANGUAGE.GERMAN,          alt_id = nil,                   strings = "german" },
+    {id = LANGUAGE.ITALIAN,         alt_id = nil,                   strings = "italian" },  
+    {id = LANGUAGE.PORTUGUESE_BR,   alt_id = LANGUAGE.PORTUGUESE,   strings = "portuguese" },
+    {id = LANGUAGE.POLISH,          alt_id = nil,                   strings = "polish" },
+    {id = LANGUAGE.RUSSIAN,         alt_id = nil,                   strings = "russian" }, -- Russian strings are very long (often the longest), and the characters in the font are big. Bad combination.
+    {id = LANGUAGE.KOREAN,          alt_id = nil,                   strings = "korean" },
+    {id = LANGUAGE.CHINESE_S,       alt_id = LANGUAGE.CHINESE_T,    strings = "chinese_s" },
+    -- {id = LANGUAGE.CHINESE_S_RAIL,  alt_id = nil,                   strings = "chinese_r" }, -- CY: not implemented
+    -- {id = LANGUAGE.JAPANESE,      alt_id = nil,                   strings = "japanese" },
+    -- {id = LANGUAGE.CHINESE_T,     alt_id = nil,                   strings = "chinese_t" },  
 }
 
-STRINGS.CHARACTERS.WALANI.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Thanks for keeping me safe, light!",
-    ON = "Pretty sure that's a lamp."
-}
 
-STRINGS.CHARACTERS.WARBUCKS.DESCRIBE.CITY_LAMP = {
-    GENERIC = "What an intriguing civilization!",
-    ON = "No need for light in the daytime, I suppose."
-}
+local langID = _G.Profile:GetLanguageID()
 
-STRINGS.CHARACTERS.WARLY.DESCRIBE.CITY_LAMP = {
-    GENERIC = "It's a small comfort.",
-    ON = "What a quaint street lamp."
-}
+modimport("languages/strings.lua")
 
-STRINGS.CHARACTERS.WATHGRITHR.DESCRIBE.CITY_LAMP = {
-    GENERIC = "It shines bright this night.",
-    ON = "Nary a glimmer."
-}
+for _, v in pairs(localizations) do
+    if(v.id == langID or v.alt_id == langID) then
+        modimport("languages/" .. v.strings .. ".lua")
+        break
+    end
+end
 
-STRINGS.CHARACTERS.WAXWELL.DESCRIBE.CITY_LAMP = {
-    GENERIC = "A welcome sign of civilization.",
-    ON = "It eases my mind."
-}
-
-STRINGS.CHARACTERS.WEBBER.DESCRIBE.CITY_LAMP = {
-    GENERIC = "They don't turn off when I get close!",
-    ON = "I miss street lamps."
-}
-
-STRINGS.CHARACTERS.WENDY.DESCRIBE.CITY_LAMP = {
-    GENERIC = "It cannot ward off the darkness forever.",
-    ON = "A place to hold light."
-}
-
-STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Quite radiant.",
-    ON = "A dormant street lamp."
-}
-
-STRINGS.CHARACTERS.WILBA.DESCRIBE.CITY_LAMP = {
-    GENERIC = "'TIS A LAMP",
-    ON = "'TIS ONLY ALIGHT AT NIGHT"
-}
-
-STRINGS.CHARACTERS.WILLOW.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Fire is so versatile.",
-    ON = "They're kinda dull in the daytime."
-}
-
-STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Wolfgang does not like the dark.",
-    ON = "Is lamp for night lights."
-}
-
-STRINGS.CHARACTERS.WOODIE.DESCRIBE.CITY_LAMP = {
-    GENERIC = "A little bit of safe haven.",
-    ON = "Looks like a street lamp."
-}
-
-STRINGS.CHARACTERS.WOODLEGS.DESCRIBE.CITY_LAMP = {
-    GENERIC = "Sanctuary!",
-    ON = "A landlamp."
-}
-
-STRINGS.CHARACTERS.WX78.DESCRIBE.CITY_LAMP = {
-    GENERIC = "PRIMITIVE SOURCE OF ILLUMINATION",
-    ON = "NONFUNCTIONING"
-}
