f7cf54aaa297ebb36ee05ea3f4c773594b493164
Cloudy Young <cloudy.young@outlook.com>
2021-11-29 07:02:34 -0700

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -13,13 +13,13 @@ author = "Cloudy Young"
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 10
 
-dst_compatible = true
-
---This lets clients know if they need to get the mod from the Steam Workshop to join the game
-all_clients_require_mod = true
-
 --This determines whether it causes a server to be marked as modded (and shows in the mod list)
 client_only_mod = false
+dst_compatible = true
+server_only_mod = true
+all_clients_require_mod = true
+
+
 
 --This lets people search for servers with this mod by these tags
 server_filter_tags = {"hamlet"}
