b07a87dd6b5d397b4d3d5d861bac7bd3dc775aef
Cloudy Young <cloudy.young@outlook.com>
2021-11-28 19:11:09 -0700

    Init commit

---
+++ modinfo.lua
@@ -0,0 +1,32 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "Hamlet City Lamp"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "0.0.1"
+
+--A description of the mod.
+description = "Hamlet City Lamp"
+
+--Who wrote this awesome mod?
+author = "Cloudy Young"
+
+--This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
+api_version = 10
+
+dst_compatible = true
+
+--This lets clients know if they need to get the mod from the Steam Workshop to join the game
+all_clients_require_mod = true
+
+--This determines whether it causes a server to be marked as modded (and shows in the mod list)
+client_only_mod = false
+
+--This lets people search for servers with this mod by these tags
+server_filter_tags = {"hamlet"}
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+forumthread = ""
+
+configuration_options = {}
